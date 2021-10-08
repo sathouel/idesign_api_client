5b242c8e52444fac4cd28f46f6c965a7fe7c5fd1
Steven Athouel <sathouel@gmail.com>
2021-10-08 15:47:00 +0300

    files added

--- idesign_api_client/config.py
+++ idesign_api_client/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Settings:
-    BASE_URL = os.environ['API_BASE_URL'] if os.environ['API_BASE_URL'] else 'https://api.international-design.pro/'
-    SANDBOX_BASE_URL = os.environ['API_SANDBOX_BASE_URL'] if os.environ['API_SANDBOX_BASE_URL'] else 'https://api.international-design.pro/'
+    BASE_URL = os.environ.get('API_BASE_URL', 'https://api.international-design.pro')
+    SANDBOX_BASE_URL = os.environ.get('API_SANDBOX_BASE_URL', 'https://api-sandbox.international-design.pro')
 
 settings = Settings()
